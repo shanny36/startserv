bfb7e498d39048e4a8569ab772696f4513752527
shanny36 <57045247+shanny36@users.noreply.github.com>
2020-02-25 03:44:34 +1000

    Update wrg.sh

--- wrg.sh
+++ wrg.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # sources .bashrc alias + binds
-cd && rm .bashrc ; wget https://github.com/shanny36/startserv/raw/master/.bashrc && source .bashrc
+apt update && cd && rm .bashrc ; wget https://github.com/shanny36/startserv/raw/master/.bashrc && source .bashrc
 
 # Update the system
-qup2 && apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common nano bash-completion sudo ufw git lsb-release wget qrencode
+up2 && apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common nano bash-completion sudo ufw git lsb-release wget qrencode
 
 # Add the Repo and Install Wireguard
-echo 'Add the repository and Install Wireguard'
-sleep 1
+#echo 'Add the repository and Install Wireguard'
+#sleep 1
 
-add-apt-repository -y ppa:wireguard/wireguard
+#add-apt-repository -y ppa:wireguard/wireguard
 
-apt update
+#apt update
 
-apt install wireguard -y
+#apt install wireguard -y
 
 # remove dnsmasq as it will run inside the container
 echo ''
@@ -63,11 +63,3 @@ sleep 1
 curl https://raw.githubusercontent.com/complexorganizations/wireguard-manager/master/wireguard-server.sh --create-dirs -o /etc/wireguard/wireguard-server.sh
 
 chmod +x /etc/wireguard/wireguard-server.sh
-
-echo ''
-echo ''
-
-sleep 5
-
-echo ''
-echo ''
